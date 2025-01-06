a437d936e812680749bffacbcdc283fb9f81c0c7
Zandré Broodryk <z.broodryk@gmail.com>
2025-01-06 17:12:03 +0200

    day 15 solution

--- src/day15/mod.rs
+++ src/day15/mod.rs
@@ -1,5 +1,6 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
+use peroxide::fuga::max;
 use regex::{Match, Regex};
 
 #[derive(PartialEq, Debug)]
@@ -10,21 +11,23 @@ enum Direction {
     Right,
 }
 
-#[derive(PartialEq, Clone, Copy)]
+#[derive(PartialEq, Clone, Copy, Eq, Hash)]
 enum TileType {
     Wall,
     Empty,
     Box,
+    BoxLeft,
+    BoxRight,
     Robot,
 }
 
-#[derive(PartialEq, Clone, Copy)]
+#[derive(PartialEq, Clone, Copy, Eq, Hash)]
 struct Coordinate {
     x: usize,
     y: usize,
 }
 
-#[derive(PartialEq, Clone, Copy)]
+#[derive(PartialEq, Clone, Copy, Eq, Hash)]
 struct WarehouseTile {
     coordinate: Coordinate,
     contents: TileType,
@@ -60,6 +63,31 @@ fn read_input() -> (Vec<WarehouseTile>, Vec<Direction>) {
     }
 
     return (warehouse_floor, moves);
+}
+
+fn double_width_on_input(input: Vec<WarehouseTile>) -> Vec<WarehouseTile> {
+    let mut new_floor = vec![];
+
+    for tile in input {
+        let mut new_tile_1 = tile.clone();
+        new_tile_1.coordinate.x *= 2;
+        let mut new_tile_2 = new_tile_1.clone();
+        new_tile_2.coordinate.x += 1;
+
+        match tile.contents {
+            TileType::Box => {
+                new_tile_1.contents = TileType::BoxLeft;
+                new_tile_2.contents = TileType::BoxRight;
+            }
+            TileType::Robot => new_tile_2.contents = TileType::Empty,
+            _ => {}
+        }
+
+        new_floor.push(new_tile_1);
+        new_floor.push(new_tile_2);
+    }
+
+    return new_floor;
 }
 
 fn map_char_to_direction(char: &str) -> Direction {
@@ -155,9 +183,256 @@ fn move_robot_in_direction(floor: &mut Vec<WarehouseTile>, direction: &Direction
     }
 }
 
+fn move_robot_in_direction_wide(floor: &mut Vec<WarehouseTile>, direction: &Direction) {
+    let floor_before_move = floor.clone();
+    let robot_tile = floor_before_move
+        .iter()
+        .find(|tile| tile.contents == TileType::Robot)
+        .unwrap();
+
+    let next_tile_to_investigate = floor_before_move
+        .iter()
+        .find(|tile| {
+            tile.coordinate == get_next_coordinate_in_direction(robot_tile.coordinate, direction)
+        })
+        .unwrap();
+
+    let mut tiles_to_move = HashSet::new();
+
+    tiles_to_move.insert(*robot_tile);
+
+    let mut tiles_to_investigate = vec![next_tile_to_investigate];
+
+    loop {
+        let mut next_tiles_to_investigate = vec![];
+        for next_tile in tiles_to_investigate.clone() {
+            match next_tile.contents {
+                TileType::Wall => return,
+                TileType::Empty => {
+                    tiles_to_move.insert(*next_tile);
+                }
+                TileType::BoxLeft => {
+                    let other_half_of_box = floor_before_move
+                        .iter()
+                        .find(|tile| {
+                            tile.coordinate
+                                == get_next_coordinate_in_direction(
+                                    next_tile.coordinate,
+                                    &Direction::Right,
+                                )
+                        })
+                        .unwrap();
+
+                    let next_tile_1 = floor_before_move
+                        .iter()
+                        .find(|tile| {
+                            tile.coordinate
+                                == get_next_coordinate_in_direction(next_tile.coordinate, direction)
+                        })
+                        .unwrap();
+
+                    let next_tile_2 = floor_before_move
+                        .iter()
+                        .find(|tile| {
+                            tile.coordinate
+                                == get_next_coordinate_in_direction(
+                                    other_half_of_box.coordinate,
+                                    direction,
+                                )
+                        })
+                        .unwrap();
+
+                    tiles_to_move.insert(*next_tile);
+                    tiles_to_move.insert(*other_half_of_box);
+                    next_tiles_to_investigate.push(next_tile_1);
+                    next_tiles_to_investigate.push(next_tile_2);
+                }
+                TileType::BoxRight => {
+                    let other_half_of_box = floor_before_move
+                        .iter()
+                        .find(|tile| {
+                            tile.coordinate
+                                == get_next_coordinate_in_direction(
+                                    next_tile.coordinate,
+                                    &Direction::Left,
+                                )
+                        })
+                        .unwrap();
+
+                    let next_tile_1 = floor_before_move
+                        .iter()
+                        .find(|tile| {
+                            tile.coordinate
+                                == get_next_coordinate_in_direction(next_tile.coordinate, direction)
+                        })
+                        .unwrap();
+
+                    let next_tile_2 = floor_before_move
+                        .iter()
+                        .find(|tile| {
+                            tile.coordinate
+                                == get_next_coordinate_in_direction(
+                                    other_half_of_box.coordinate,
+                                    direction,
+                                )
+                        })
+                        .unwrap();
+
+                    tiles_to_move.insert(*next_tile);
+                    tiles_to_move.insert(*other_half_of_box);
+                    next_tiles_to_investigate.push(next_tile_1);
+                    next_tiles_to_investigate.push(next_tile_2);
+                }
+                _ => todo!(),
+            }
+        }
+        if next_tiles_to_investigate.len() == 0 {
+            break;
+        }
+
+        tiles_to_investigate = next_tiles_to_investigate
+            .iter()
+            .filter(|tile_to_investigate| tiles_to_move.get(&tile_to_investigate) == None)
+            .map(|tile| *tile)
+            .collect();
+    }
+
+    move_tiles(floor, tiles_to_move, direction);
+}
+
+fn move_tiles(
+    floor: &mut Vec<WarehouseTile>,
+    tiles: HashSet<WarehouseTile>,
+    direction: &Direction,
+) {
+    for tile in tiles.clone() {
+        let tile = floor
+            .iter_mut()
+            .find(|floor_tile| **floor_tile == tile)
+            .unwrap();
+
+        let last_in_slice_in_direction =
+            find_last_coordinate_in_slice_in_direction(tile, &tiles, direction);
+
+        if tile.coordinate == last_in_slice_in_direction {
+            let first_coordinate =
+                find_first_coordinate_in_slice_in_direction(tile, &tiles, direction);
+            tile.coordinate = first_coordinate;
+        } else {
+            tile.coordinate = get_next_coordinate_in_direction(tile.coordinate, direction)
+        }
+    }
+}
+
+fn find_first_coordinate_in_slice_in_direction(
+    tile: &WarehouseTile,
+    tiles: &HashSet<WarehouseTile>,
+    direction: &Direction,
+) -> Coordinate {
+    match direction {
+        Direction::Up => {
+            let mut current_tile = tile;
+            while let Some(previous_tile) = tiles.iter().find(|floor_tile| {
+                floor_tile.coordinate.y == current_tile.coordinate.y + 1
+                    && floor_tile.coordinate.x == current_tile.coordinate.x
+            }) {
+                current_tile = previous_tile;
+            }
+
+            return current_tile.coordinate;
+        }
+        Direction::Down => {
+            let mut current_tile = tile;
+            while let Some(previous_tile) = tiles.iter().find(|floor_tile| {
+                floor_tile.coordinate.y == current_tile.coordinate.y - 1
+                    && floor_tile.coordinate.x == current_tile.coordinate.x
+            }) {
+                current_tile = previous_tile;
+            }
+
+            return current_tile.coordinate;
+        }
+        Direction::Left => {
+            let mut current_tile = tile;
+            while let Some(previous_tile) = tiles.iter().find(|floor_tile| {
+                floor_tile.coordinate.x == current_tile.coordinate.x + 1
+                    && floor_tile.coordinate.y == current_tile.coordinate.y
+            }) {
+                current_tile = previous_tile;
+            }
+
+            return current_tile.coordinate;
+        }
+        Direction::Right => {
+            let mut current_tile = tile;
+            while let Some(previous_tile) = tiles.iter().find(|floor_tile| {
+                floor_tile.coordinate.x == current_tile.coordinate.x - 1
+                    && floor_tile.coordinate.y == current_tile.coordinate.y
+            }) {
+                current_tile = previous_tile;
+            }
+
+            return current_tile.coordinate;
+        }
+    }
+}
+
+fn find_last_coordinate_in_slice_in_direction(
+    tile: &WarehouseTile,
+    tiles: &HashSet<WarehouseTile>,
+    direction: &Direction,
+) -> Coordinate {
+    match direction {
+        Direction::Up => {
+            let mut current_tile = tile;
+            while let Some(previous_tile) = tiles.iter().find(|floor_tile| {
+                floor_tile.coordinate.y == current_tile.coordinate.y - 1
+                    && floor_tile.coordinate.x == current_tile.coordinate.x
+            }) {
+                current_tile = previous_tile;
+            }
+
+            return current_tile.coordinate;
+        }
+        Direction::Down => {
+            let mut current_tile = tile;
+            while let Some(previous_tile) = tiles.iter().find(|floor_tile| {
+                floor_tile.coordinate.y == current_tile.coordinate.y + 1
+                    && floor_tile.coordinate.x == current_tile.coordinate.x
+            }) {
+                current_tile = previous_tile;
+            }
+
+            return current_tile.coordinate;
+        }
+        Direction::Left => {
+            let mut current_tile = tile;
+            while let Some(previous_tile) = tiles.iter().find(|floor_tile| {
+                floor_tile.coordinate.x == current_tile.coordinate.x - 1
+                    && floor_tile.coordinate.y == current_tile.coordinate.y
+            }) {
+                current_tile = previous_tile;
+            }
+
+            return current_tile.coordinate;
+        }
+        Direction::Right => {
+            let mut current_tile = tile;
+            while let Some(previous_tile) = tiles.iter().find(|floor_tile| {
+                floor_tile.coordinate.x == current_tile.coordinate.x + 1
+                    && floor_tile.coordinate.y == current_tile.coordinate.y
+            }) {
+                current_tile = previous_tile;
+            }
+
+            return current_tile.coordinate;
+        }
+    }
+}
+
 fn print_board(floor: &Vec<WarehouseTile>) {
     let mut board_string = String::new();
-
+    let mut max_x = 0;
     let mut y = 0;
     loop {
         let mut x = 0;
@@ -172,10 +447,16 @@ fn print_board(floor: &Vec<WarehouseTile>) {
                     TileType::Empty => '.',
                     TileType::Box => 'O',
                     TileType::Robot => '@',
+                    TileType::BoxLeft => '[',
+                    TileType::BoxRight => ']',
                 };
                 board_string.push(char_to_add);
                 x += 1;
+                max_x = max(vec![x, max_x]);
             } else {
+                if x < max_x {
+                    panic!("something terrible happened")
+                }
                 break;
             }
         }
@@ -200,7 +481,7 @@ fn calculate_gps_coordinate(coordinate: Coordinate) -> usize {
 fn calculate_total_gps_score(floor: &Vec<WarehouseTile>) {
     let boxes = floor
         .iter()
-        .filter(|tile| tile.contents == TileType::Box)
+        .filter(|tile| tile.contents == TileType::Box || tile.contents == TileType::BoxLeft)
         .map(|tile| calculate_gps_coordinate(tile.coordinate))
         .sum::<usize>();
 
@@ -214,6 +495,22 @@ pub fn day_15_part_1() {
 
     for direction in moves {
         move_robot_in_direction(&mut floor, &direction);
+        // print_board(&floor);
+    }
+
+    print_board(&floor);
+    calculate_total_gps_score(&floor);
+}
+
+pub fn day_15_part_2() {
+    let (mut floor, moves) = read_input();
+
+    floor = double_width_on_input(floor);
+
+    print_board(&floor);
+
+    for direction in moves {
+        move_robot_in_direction_wide(&mut floor, &direction);
         // print_board(&floor);
     }
 
